5d6e498670fc9c09f2c5936d65c84d45b4f8a42c
abdulsamet123 <abdul.kurnaz@hotmail.com>
2020-12-10 14:11:40 +0100

    test

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/" , name="homepage")
+     */
+    public function homepageAction(){
+        return new Response("<h1>Welkom</h1>");
+        // return $this->render('admin/homepage.html.twig');
+    }
+}
